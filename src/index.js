ae5d85e007941c6c233b416d2e3d6f49af5489b6
ck2e14 <chriswkennedy@icloud.com>
2019-11-05 14:49:10 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
+saySup = () => {
+  console.log('sup')
+}
+
+
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.saySup} />
   </div>,
   document.getElementById('root')
 );
